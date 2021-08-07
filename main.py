1004d91c97c8ea7cd0d7d601f75d73fda891d64d
Bishal B K <60055497+theperrinpages@users.noreply.github.com>
2021-08-08 03:23:24 +1000

    the_learning_log_program

---
+++ main.py
@@ -0,0 +1,49 @@
+# This is my learning tracking program.
+import datetime
+import os
+
+lists = {}
+
+
+def courses_in():  # updates the database with today's learnt items
+    print("Type exit when done")
+    x = ""
+    while x != "exit":
+        x = input("Add entries to the learning database > ").lower()
+        if x == "exit":
+            break
+        lists[x] = time_func()
+    return lists
+
+
+def show_courses():
+    if os.name == 'nt':
+        os.system("cls")  # windows compatibility
+    else:
+        os.system("clear")  # linux compatibility
+
+    print("List of items learnt today:")
+    for x, y in lists.items():
+        strings = "\t" + x + " @ " + str(y) + "\n"
+        print(strings)
+        file_create(strings)
+
+
+def time_func():
+    time_now = datetime.datetime.now()
+    return time_now
+
+
+def file_create(log):
+    f = open("test.log", "a")
+    f.write(log)
+    f.close()
+    print("Insertion successful! :)")
+
+    g = open("test.log", "r")
+    print(g.read())
+    f.close()
+
+
+courses_in()
+show_courses()
